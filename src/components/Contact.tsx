0e06ca7cbe4d652308f431b1e05e5f0c5a63a34b
Fath <fathzulfa34@gmail.com>
2023-06-07 16:02:46 +0700

    Event Handle Is

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 //type props bisa dipakai untuk variabel
 type Props = {
@@ -7,21 +7,33 @@ type Props = {
 }
                                 // ini adalah variabel
 const Contact: React.FC<Props> = ({ name, phone }) => {
+    const [isPhoneCensored, setIsPhoneCensored] = useState(false)
+    
     return (
 //style dalam div disebut prop/properties
         <div style={{
             padding: "16px 0px",
             borderBottom: "1px solid rgba(0, 0, 128, 0.5)"
-        }}>
+        }}
+        onClick={() => setIsPhoneCensored(!isPhoneCensored)}
+        >
             <p style={{
                 fontSize: "16px",
                 color: 'black',
                 marginBottom: 8,
             }}>{name}</p>
-            <p style={{
-                fontSize: "16px",
-                color: 'black',
-            }}>{phone}</p>
+            {!isPhoneCensored && (
+                <p style={{
+                    fontSize: "16px",
+                    color: 'black',
+                }}>{phone}</p>
+            )}
+            {isPhoneCensored && (
+                <p style={{
+                    fontSize: "16px",
+                    color: 'black',
+                }}>*************</p>
+            )}
         </div>
     );
 };
