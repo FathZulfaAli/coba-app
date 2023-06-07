be6066dce5393f0c25613d4cbf57a18f53fdc005
Fath <fathzulfa34@gmail.com>
2023-06-07 15:12:08 +0700

    Create Style

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,8 +1,19 @@
 const Contact = () => {
     return (
-        <div>
-            <p>Angel Lord</p>
-            <p>089123456789</p>
+//style dalam div disebut prop/properties
+        <div style={{
+            padding: "16px 0px",
+            borderBottom: "1px solid rgba(0, 0, 128, 0.5)"
+        }}>
+            <p style={{
+                fontSize: "16px",
+                color: 'black',
+                marginBottom: 8,
+            }}>Angel Lord</p>
+            <p style={{
+                fontSize: "16px",
+                color: 'black',
+            }}>089123456789</p>
         </div>
     );
 };
